94fa3780acb11fc7638c942ece1846347452058d
AntWader <AntWader@users.noreply.github.com>
2022-07-16 18:52:41 +0300

    test server

---
+++ test.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 80
+
+let i = 0
+app.get('/', (req, res) => {
+  var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+  console.log(fullUrl);
+  i++
+  res.send(`<h1>Counter: ${i}</h1>`)
+})
+
+app.listen(port, () => {
+  console.log(`Open the following link in your browser: http://3.68.160.227:${port}`)
+})
